8dd74c74ea4028c21f41d669af2d81cb8f87c35a
jerome158 <jerome158@outlook.com>
2025-02-11 09:13:18 +0800

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -320,7 +320,7 @@ class ConfigManager:
 
     @property
     def update_time_position(self):
-        return self.config.get("Settings", "update_time_position", fallback="bot")
+        return self.config.get("Settings", "update_time_position", fallback="bottom")
 
     @property
     def time_zone(self):
